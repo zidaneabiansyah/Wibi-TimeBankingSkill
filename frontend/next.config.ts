39e9aa23640dfc93436783493ecb95e3aaea42c1
OwlDane <zidunnoaoe@gmail.com>
2026-01-01 02:50:53 +0700

    Refactor code structure for improved readability and maintainability

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa" with { type: "commonjs" };
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  sw: "/sw.js",
+})(nextConfig);
